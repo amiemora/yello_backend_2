6a5838181b67d4d54cb854912642f04f122b02ce
Amie Morales <xamiemmorales@gmail.com>
2022-09-15 15:10:42 -0500

    host

--- yello_2/settings.py
+++ yello_2/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'https://secure-forest-62515.herokuapp.com/']
 
 
 # Application definition
