e8ef102c242fef8aafe1eef31583bd2e11ecb286
Amie Morales <xamiemmorales@gmail.com>
2022-09-15 17:15:49 -0500

    final commit before templates

--- user_yello/serializers.py
+++ user_yello/serializers.py
@@ -11,4 +11,4 @@ class UserSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer): 
     class Meta:
         model = Post 
-        fields = ('id','author', 'title', 'iamge', 'description',)
+        fields = ('id','author', 'title', 'image', 'description',)
