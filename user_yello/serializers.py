ef4bdc739d4c239a9db241884bbc60a073b551cb
Amie Morales <xamiemmorales@gmail.com>
2022-09-16 10:36:01 -0500

    updae for pull

--- user_yello/serializers.py
+++ user_yello/serializers.py
@@ -1,14 +1,33 @@
 from rest_framework import serializers 
 from .models import Post
 from django.contrib.auth.models import User 
+from django.contrib.auth.hashers import make_password, check_password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id','username','email','password')
 
+    def create(self, validated_data):
+        user = User.objects.create(
+        email=validated_data['email'],
+        password = make_password(validated_data['password'])
+        )
+        user.save()
+        return user
+
+    def update(self,instance, validated_data):
+        user = User.objects.get(email=validated_data["email"])
+        user.password = make_password(validated_data["password"])
+        user.save()
+        return user
+
 
 class PostSerializer(serializers.ModelSerializer): 
     class Meta:
         model = Post 
         fields = ('author', 'title', 'image', 'description',)
+
+
+
+
