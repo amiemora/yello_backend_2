e5854d2ada3db020d7266471786a9269050e9be7
Amie Morales <xamiemmorales@gmail.com>
2022-09-16 14:09:05 -0500

    heroku push

--- user_yello/models.py
+++ user_yello/models.py
@@ -18,10 +18,10 @@ def __str__(self):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    commenenter_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    commenter_name = models.ForeignKey(User, on_delete=models.CASCADE)
     comment_body = models.TextField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
 
     def __str__(self):
-        return str(self.commenenter_name)
+        return str(self.commenter_name)
